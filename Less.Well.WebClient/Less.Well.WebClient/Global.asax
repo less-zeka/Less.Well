f7de26cf0946f0ea6d379a10ea911eddb7f815f8
less <less-zeka@gmail.com>
2015-07-05 13:00:03 +0200

    Project Scaffolded
    mvc

---
+++ Less.Well.WebClient/Less.Well.WebClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Less.Well.WebClient.MvcApplication" Language="C#" %>
---
+++ Less.Well.WebClient/Less.Well.WebClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Less.Well.WebClient.Startup))]
+namespace Less.Well.WebClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
